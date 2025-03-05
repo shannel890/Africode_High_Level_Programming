95252702ce460939cb4b8048f4285c49135b2e90
shannel890 <shannelkirui739@gmail.com>
2025-03-05 15:30:25 +0300

    added variable.py and primitive_data_types.py

---
+++ Python/Variable_Primitive_Data_Types/primitive_data_types.py
@@ -0,0 +1,33 @@
+#Number
+
+#1.Integer
+# age = 12
+# print(type(age))
+
+# #2.float
+# heigth = 1.5
+# print(type(heigth)) 
+
+# sum = age + heigth
+# print(type(sum))
+
+#Boolean
+# is_student = False
+# print(is_student)
+
+#4. string
+# student_name = "Enock"
+# teacher_name = 'Ben'
+# student_name = "kipngeno" 
+# long_string = "kipngeno" 
+# r = long_string[-3]
+# print(r)
+# long_string = "Neighbour"
+# z = long_string[-1]
+# print(z)
+# class_rep = "ROP"
+
+# y= student_name[7]
+# print(y)
+# long_string = "neighbour"
+# 
---
+++ Python/Variable_Primitive_Data_Types/variable.py
@@ -0,0 +1,53 @@
+#A Variable is a container for storing data values.
+
+#VARIABLE NAMING CONVENTIONS.
+
+#A variables name nust start with a letter or the underscore character.
+# student = "Kirui"
+# print(student)
+# _dob = "5-03-2025"
+# print(_dob)
+
+#A variable name cannot start with a number.
+# 3students = "kirui"
+
+#A variable name can only contain alpha-numeric characters and underscores(A-z,0-9,and _). 
+# student1 = "kirui"
+# student_name = "kipngeno"
+# print(student1)
+# print(student_name)
+#A variable names are case-sensitive (age,Age and AGE are three different variables).
+# age = 25
+# print("age:" , age) 
+# Age = 30
+# print("Age:" ,Age)
+# print(age)
+
+
+
+#A variable names cannot contain spaces.
+# student name = "Enock"
+# print(student name)
+#A variable name should be descriptive and meaningful (age is better than a)
+# is_student = True
+# print(is_student)
+# student_ID = 1834
+# print(student_ID)
+#Variable names cannot be a reserved keyword in python.
+# global = "world"
+#Standard naming Conventions.
+#1. Camel case 
+# student_name = "Enock"
+# print(student_name)
+#2. Pascal case
+# student_name = "ROP"
+# print(student_name)
+#3. Snake Case
+# student_name = "Dorothy"
+# print(student_name)
+
+#Variable can be overwritten/updated
+student_ID = "10"
+student_ID = "20"
+print(student_ID)
+
