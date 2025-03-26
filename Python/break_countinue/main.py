46f5ec50c0b5f1bb5e36bb5a491c67d1cd699f73
shannel890 <shannelkirui739@gmail.com>
2025-03-26 15:06:24 +0300

    added main.py and main2.py

---
+++ Python/break_countinue/main.py
@@ -0,0 +1,42 @@
+# correct_pin = "1234"
+# attempts = 0
+# max_attempts = 3
+
+# while attempts < max_attempts:
+#     pin = input("Enter your m-pesa pin")
+#     attempts += 1
+
+#     remaining_attempts = max_attempts - attempts
+#     if remaining_attempts > 0:
+#         print(f"Wrong pin entered.You have {remaining_attempts} attempts left")
+#     else:
+#         print("Account blocked!Too many attempts")
+
+#     if pin == "":
+#         print("You have not entered  pin")
+#         continue
+#     if pin == correct_pin:
+#         print("pin accepted! transaction completed")
+#         break
+# def add_two_nums(num_one,num_two):
+#      return num_one +  num_two
+
+# def greet(name):
+#      # "this greets the user. "
+#      print(f"Hello,{name}")
+#      greet("Alii")
+
+# def add(a, b):
+#      return a + b
+
+# result = add(5, 2)
+# print(result)
+
+x = 10
+def dis():
+     y = 5
+     print(y)
+
+
+
+    
---
+++ Python/break_countinue/main2.py
@@ -0,0 +1,25 @@
+# counter = 0
+
+# while counter < 20:
+#     if counter == 7:
+#         counter += 1  # Ensure counter increments before continue
+#         continue
+
+#     if counter == 18:
+#         break
+
+#     print(counter)
+#     counter += 1
+
+
+num = 0 
+while num <= 10:
+    num += 1
+    if num % 2 == 0:
+        continue
+    print(f"Odd number:{num}")
+
+    
+    
+    
+    
